5696225c8554c570ad0b5f0601ff3bfaafb40e32
Qiaozhi <ql316@cam.ac.uk>
2023-03-17 17:20:06 +0000

    powerup

--- Assets/Powerup.cs
+++ Assets/Powerup.cs
@@ -1,22 +1,37 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class Powerup : MonoBehaviour
 {
     public LayerMask pickupLayer;
+    [Header("Text Notification")] public string pickupLine;
+    public TextMeshProUGUI pickupText;
+    public Color textColor;
+
+    protected virtual void Start()
+    {
+        pickupText.enabled = false;
+        pickupText.color = textColor;
+        pickupText.text = pickupLine;
+    }
 
     private void OnTriggerEnter2D(Collider2D col)
     {
         if (LayerMaskHelper.IsLayerInLayerMask(col.gameObject.layer, pickupLayer))
         {
             OnPickUp(col);
-            Destroy(gameObject);
+            Destroy(gameObject, 3f);
         }
     }
+
     protected virtual void OnPickUp(Collider2D col)
     {
         XLogger.Log(Category.PowerUp, $"{gameObject.name} picked up by {col.gameObject.name}");
+        GetComponentInChildren<SpriteRenderer>().gameObject.SetActive(false);
+        pickupText.enabled = true;
+        pickupText.GetComponent<Animator>().Play("PickupText");
     }
 }
